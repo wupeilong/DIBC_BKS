1f3f419edb09bac3c91d18fc1b23f51956ea3caf
zhoulingzhang <867187836@qq.com>
2020-01-11 14:42:19 +0800

    14:41

---
+++ src/main/java/cn/dibcbks/controller/MainController.java
@@ -0,0 +1,17 @@
+package cn.dibcbks.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+* @Title: MainController.java
+* @Package cn.dibcbks.controller
+* @Description:(用一句话描述该文件做什么)
+* @author: ZhouLingZhang
+* @date 2020年1月11日
+* @Copyright:好前途教育
+* @version V1.0
+*/
+@Controller
+public class MainController {
+
+}
