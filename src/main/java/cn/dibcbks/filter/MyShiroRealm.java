49b625afe80d2cd71ffe82f7f38aff728a483fb6
zhoulingzhang <867187836@qq.com>
2020-03-12 23:14:56 +0800

    557

--- src/main/java/cn/dibcbks/filter/MyShiroRealm.java
+++ src/main/java/cn/dibcbks/filter/MyShiroRealm.java
@@ -5,15 +5,10 @@ package cn.dibcbks.filter;
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.AuthenticationInfo;
 import org.apache.shiro.authc.AuthenticationToken;
-import org.apache.shiro.authc.SimpleAuthenticationInfo;
 import org.apache.shiro.authz.AuthorizationInfo;
 import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.subject.PrincipalCollection;
-import org.apache.shiro.util.ByteSource;
-import org.springframework.beans.factory.annotation.Autowired;
 
-import cn.dibcbks.entity.User;
-import cn.hqtmain.service.IUserService;
 
 
 
